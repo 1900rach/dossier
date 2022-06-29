3bf2113746a961167072755063abd51ad670551c
Savhel <Blife1255@gmail.com>
2022-06-29 01:55:12 +0100

    j'ai augmenter la taille de ton champs password dans la table utilisateur et j'ai remarqué que tu utiliste MATRICULE qui n'est pas un champs de ta table donc j'ai supprimé et j'ai remplacé par ID

--- polytechnique.sql
+++ polytechnique.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  mer. 29 juin 2022 à 07:38
--- Version du serveur :  10.4.8-MariaDB
--- Version de PHP :  7.3.11
+-- Host: localhost:3306
+-- Generation Time: Jun 28, 2022 at 11:19 PM
+-- Server version: 5.7.33
+-- PHP Version: 8.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `polytechnique`
+-- Database: `polytechnique`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `these`
+-- Table structure for table `these`
 --
 
 CREATE TABLE `these` (
@@ -40,7 +39,7 @@ CREATE TABLE `these` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
 CREATE TABLE `utilisateur` (
@@ -49,33 +48,34 @@ CREATE TABLE `utilisateur` (
   `PRENOM` varchar(50) NOT NULL,
   `DATE_NAIS` date NOT NULL,
   `DEPART` varchar(30) NOT NULL,
-  `PASSWORD` varchar(20) NOT NULL,
+  `PASSWORD` varchar(100) NOT NULL,
   `NUM_TEL` int(11) NOT NULL,
   `EMAIL` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `utilisateur`
+-- Dumping data for table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`ID`, `NOM`, `PRENOM`, `DATE_NAIS`, `DEPART`, `PASSWORD`, `NUM_TEL`, `EMAIL`) VALUES
 ('09rac99el', 'TSAFACK fotso', 'SAVIO', '1999-07-09', 'GIN', '$2y$10$7gB4smNpLbbWD', 690295069, 'blife1255@gmail.com'),
+('123AZER', 'blhack', 'resu', '2022-06-08', 'GIN', '$2y$10$wV1rGchySxPco.zzOy0IN.hJPJerXYfjHat57Sc3dtPHRMRBkT2Xy', 690295069, 'blife1255@gmail.com'),
 ('28sav03io', 'nzoba nzogue', 'rachel onyx', '2000-03-28', 'GIN', '$2y$10$PrgBptYTpdvB9', 698247700, 'rachelnzogue9@gmail.com'),
 ('re02beca', 'nzogue', 'rebecca', '2002-08-02', 'GC', '$2y$10$xMInRRxO9O0xv', 658601030, 'brivaeil@gmail.com'),
 ('ru98thmak', 'makougang', 'ruth', '1998-03-28', 'GTEL/GEL', '$2y$10$yf0kbaQSsox8z', 695678402, 'ruth@gmail.com');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `these`
+-- Indexes for table `these`
 --
 ALTER TABLE `these`
   ADD PRIMARY KEY (`ID_T`);
 
 --
--- Index pour la table `utilisateur`
+-- Indexes for table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`ID`);
